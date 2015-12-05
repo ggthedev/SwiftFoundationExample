44ddece20557fb56d07228fef03d275b3aad967f
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-12-05 18:50:16 -0500

    working on main.swift

--- SwiftFoundationExample/SwiftFoundationExample/main.swift
+++ SwiftFoundationExample/SwiftFoundationExample/main.swift
@@ -6,9 +6,15 @@
 //  Copyright © 2015 PureSwift. All rights reserved.
 //
 
+#if os(OSX)
+import Foundation
+#endif
+
 import SwiftFoundation
 
 print("Hello, World!")
 
+let uuid = UUID()
 
+print("Random UUID: " + uuid.rawValue)
 
